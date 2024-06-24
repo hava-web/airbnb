d15b6f83d17fae65096adad53a5edfe448a8812b
hava-web <nguyenngocha18062002@gmail.com>
2024-06-24 07:21:41 +0700

    add mongo

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express';
-
 import cors, { CorsOptions } from 'cors';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { UserModel } from './models/User';
+require('dotenv').config();
 
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+// const bcryptSalt = bcrypt.genSalt(10);
 
 const corsOption: CorsOptions = {
   credentials: true,
@@ -10,6 +16,7 @@ const corsOption: CorsOptions = {
 };
 
 //mongo user password: hE0LQmloS6dKnZwm
+mongoose.connect(process.env.MONGO_URL ?? '');
 
 app.use(express.json());
 app.use(cors(corsOption));
@@ -18,9 +25,15 @@ app.get('/', (req: Request, res: Response) => {
   return res.json('Hello');
 });
 
-app.post('/register', (req: Request, res: Response) => {
+app.post('/register', async (req: Request, res: Response) => {
   const { name, email, password }: { name: string; email: string; password: string } = req.body;
-  res.json({ name, email, password });
+  const userDoc = await UserModel.create({
+    name,
+    email,
+    password: bcrypt.hashSync(password, bcryptSalt),
+  });
+
+  res.json(userDoc);
 });
 
 app.listen(4000);
