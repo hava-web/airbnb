c53d54735e7c3867d8e7d605e24f7c9d8e34d930
hava-web <nguyenngocha18062002@gmail.com>
2024-07-22 00:46:18 +0700

    add

--- airbnb-clone/src/pages/LoginPage.tsx
+++ airbnb-clone/src/pages/LoginPage.tsx
@@ -1,37 +1,43 @@
 import { ChangeEvent, FC, FormEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import accountServices from '../services/account';
 
 const LoginPage: FC = () => {
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
-  
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
   const onSubmit = async (e: FormEvent) => {
     try {
       e.preventDefault();
-      await accountServices.Login({email, password});
+      await accountServices.Login({ email, password });
       alert('Login success');
+      setIsLogin(true);
     } catch (error) {
       alert('Login failed');
-    } 
+    }
   };
+
+  if (isLogin) {
+    return <Navigate to={'/'} />;
+  }
 
   return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='mb-64'>
         <h1 className='text-3xl text-center mb-4'>Login</h1>
         <form className='max-w-md mx-auto' onSubmit={onSubmit}>
-          <input 
-            type='email' 
-            placeholder='your@email.com' 
+          <input
+            type='email'
+            placeholder='your@email.com'
             value={email}
             onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
           />
-          <input 
-            type='password' 
+          <input
+            type='password'
             placeholder='password'
             value={password}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} 
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
           />
           <button className='primary'>Login</button>
           <div className='text-center py-2 text-gray-500'>
