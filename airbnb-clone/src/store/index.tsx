0aeb476016164c001af3a183c0728ce750fca15e
hava-web <nguyenngocha18062002@gmail.com>
2024-07-24 07:27:40 +0700

    config redux and saga

---
+++ airbnb-clone/src/store/index.tsx
@@ -0,0 +1,19 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+const rootReducer = combineReducers({});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
